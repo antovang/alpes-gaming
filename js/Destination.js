137333b5130dcd4c59a47dfa0e0ef704b95dbe25
Antonio Van Grieken <antovang@gmail.com>
2022-01-05 10:50:59 +0100

    Commit TP2

--- js/Destination.js
+++
@@ -1,9 +0,0 @@
-class Destination {
-
-    constructor(srcImg,nomPays,description,prix){
-        this.srcImage = srcImg;
-        this.pays = nomPays;
-        this.descriptionPromo = description;
-        this.prixPromo = prix;
-    }
-}
